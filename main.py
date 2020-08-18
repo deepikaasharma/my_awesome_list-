60838a3679f41bf233a0aa6628d2f7391da36aae
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-18 04:39:04 +0000

    created a repetitive list

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-# Create a list named my_awesome_list that contains the uppercase letters 'A', 'B', 'C', 'D' and 'E' in alphabetical order.
-my_awesome_list = ['A', 'B', 'C', 'D', 'E']
+# # Create a list named my_awesome_list that contains the uppercase letters 'A', 'B', 'C', 'D' and 'E' in alphabetical order.
+# my_awesome_list = ['A', 'B', 'C', 'D', 'E']
+
+"""Create a list named my_repetitive_list that contains the sequence of uppercase letters 'A', 'B', 'C', 'D' and 'E' in alphabetical order, repeated 15 times. See below for example:
+
+['A', 'B', 'C', 'D', 'E', ... , 'D', 'E']
+
+"""
+my_repetitive_list = ['A', 'B', 'C', 'D', 'E']*15
+
+print(my_repetitive_list)
